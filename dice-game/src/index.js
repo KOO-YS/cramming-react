79f2a59fa444f4eaa5fa73b991036fe4f451c58d
yaans <kbeeysk@naver.com>
2023-01-21 22:40:41 +0900

    apply fragment tag

--- dice-game/src/index.js
+++ dice-game/src/index.js
@@ -1,7 +1,19 @@
+import { Fragment } from 'react';
 import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const contraction = ReactDOM.createRoot(document.getElementById('contraction'));
 
 root.render(
-  <h1>실행 테스트</h1>
+  <Fragment>
+    <h1>실행 테스트</h1>
+    <h3>두 줄 이상의 테스트 코드</h3>
+  </Fragment>
+);
+
+contraction.render(
+  <>
+    <h1>contraction - 실행 테스트</h1>
+    <h3>contraction - 두 줄 이상의 테스트 코드</h3>
+  </>
 );
