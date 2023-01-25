3f56b086ef4e5840916e8ed10f22a5a374a92f4d
yeonsookoo <yeonsoo.koo@bespinglobal.com>
2023-01-25 20:08:45 +0900

    tidy up existed code

--- dice-game/src/index.js
+++ dice-game/src/index.js
@@ -1,26 +1,7 @@
-import { Fragment } from 'react';
 import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const product = 'candy';
-const flavour = 'strawberry';
-
-const productLabel = product.toUpperCase() + ' ' + flavour;
-const productImg = 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Candy_in_Damascus.jpg/250px-Candy_in_Damascus.jpg';
-
-function handleClick() {
-  alert('구매 성공');
-} 
-
 root.render(
-  <Fragment>
-    {/* <h1>판매 : {product.toUpperCase() + ' '   + flavour} </h1> */}
-    <h1>판매 : {productLabel} </h1>
-    <img src={productImg} alt='productImg'/>
-    
-    <br></br>
-    
-    <button onClick={handleClick}>구매하기</button>
-  </Fragment>
+  <h1>Dice Game</h1>
 );
